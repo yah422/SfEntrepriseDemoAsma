b2e0ccbe20fcdfbd832d9569ff3507e3217619de
AsmaS <saidiasma422@gmail.com>
2024-09-18 10:53:10 +0200

    add a public function to the employeController to show employe detail and create a new file to display it

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Employe;
 use App\Repository\EmployeRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -18,4 +19,15 @@ class EmployeController extends AbstractController
             'employes' => $employes
         ]);
     }
+
+    #[Route('/employe/{id}', name: 'show_employe')]
+    public function show(Employe $employe): Response
+    {
+        return $this->render('employe/show.html.twig', [
+            'employe' => $employe
+
+        ]);
+
+    }
+
 }
